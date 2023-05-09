1487f370a254c9c72699ae472f8f888a55f9e511
valeriofilareti <valerio.filareti@gmail.com>
2023-05-09 14:07:41 +0200

    [add] jumbotron

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -4,11 +4,56 @@ name: 'myMain'
 }
 </script>
 <template>
-<h1>Main</h1>
+  <div class="container">
+    <div class="infos d-flex justify-between">
+      <div class="element d-flex">
+        <div class="img">
+          <img src="../assets/img/truck.png" alt="">
+        </div>
+        <div class="text">
+          <span>Free Delivery</span>
+          <span>Free Shipping On All Order</span>
+        </div>
+      </div>
+      <div class="elemen d-flex">
+        <div class="img">
+          <img src="../assets/img/truck.png" alt="">
+        </div>
+        <div class="text">
+          <span>Free Delivery</span>
+          <span>Free Shipping On All Order</span>
+        </div>
+      </div>
+      <div class="element d-flex">
+        <div class="img">
+          <img src="../assets/img/truck.png" alt="">
+        </div>
+        <div class="text">
+          <span>Free Delivery</span>
+          <span>Free Shipping On All Order</span>
+        </div>
+      </div>
+      <div class="element d-flex">
+        <div class="img">
+          <img src="../assets/img/truck.png" alt="">
+        </div>
+        <div class="text">
+          <span>Free Delivery</span>
+          <span>Free Shipping On All Order</span>
+        </div>
+      </div>
+      <div class="element"></div>
+      <div class="element"></div>
+      <div class="element"></div>
+    </div>
+  </div>
 </template>
 
 
 <style lang="scss" scoped>
+span {
+  display: block;
+}
 
 
 
