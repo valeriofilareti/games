e9156c94c9c493a3854f2ac59dae030fb6da9e1e
valeriofilareti <valerio.filareti@gmail.com>
2023-05-09 15:33:17 +0200

    [add] jumbotron

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -3,7 +3,9 @@ export default {
 name: 'myMain'
 }
 </script>
+
 <template>
+  <!-- infos --------------------------------------------->
   <div class="container">
     <div class="infos d-flex justify-between">
       <div class="element d-flex">
@@ -46,13 +48,44 @@ name: 'myMain'
       <div class="element"></div>
       <div class="element"></div>
     </div>
+    <!-- infos ----------------------------------------->
+    <!-- latest-popular -------------------------------->
+    <div class="boxes">
+      <div class="latest">
+        <div class="text">
+          <span class="yellow-box">most popular</span>
+          <span>battlefield</span>
+        </div>
+      </div>
+      <div class="popular">
+        <div class="text">
+          <span class="yellow-box">latest game</span>
+          <span>assassins</span>
+        </div>
+      </div>
+    </div>
+    <!-- latest-popular -------------------------------->
   </div>
 </template>
 
 
 <style lang="scss" scoped>
+@import '../scss/main.scss';
 span {
-  display: block;
+  display: inline-block;
+}
+.latest, .popular {
+  width: 50%;
+  height: 350px;
+  border: 1px solid white;
+  float: left;
+  padding: 10px;
+}
+.latest {
+  background-image: url(../assets/img/cms-banner-01.jpg);
+}
+.popular {
+  background-image: url(../assets/img/cms-banner-02.jpg);
 }
 
 
