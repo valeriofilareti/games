a73fe21d94bd6bebabdc2e87eadcbcc948aeb0eb
valeriofilareti <valerio.filareti@gmail.com>
2023-05-09 11:54:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+  <script>
+  export default {
+  
+  }
+  </script>
+
+<template>
+  <body>
+    
+  </body>
+</template>
+
+
+<style lang="scss">
+body {
+  background-image: url(./assets/img/body-bg.jpg);
+}
+</style>
